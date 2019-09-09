24c8a071ef95db914c61af77b0cd12972b67215f
Noah <noah.colbourne@ocsbstudent.ca>
2019-09-09 12:24:38 -0400

    Added Program Menu

---
+++ ProgramMenuNoah/ProgramMenuNoah/ProgramMenuForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ProgramMenuNoah
+{
+    partial class frmProgramMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCallMeCarson = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniProgram = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCallMeCarson
+            // 
+            this.lblCallMeCarson.AutoSize = true;
+            this.lblCallMeCarson.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCallMeCarson.Location = new System.Drawing.Point(231, 43);
+            this.lblCallMeCarson.Name = "lblCallMeCarson";
+            this.lblCallMeCarson.Size = new System.Drawing.Size(332, 60);
+            this.lblCallMeCarson.TabIndex = 0;
+            this.lblCallMeCarson.Text = "Call Me Carson";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProgramMenuNoah.Properties.Resources.callmecarsoncrying;
+            this.pictureBox1.Location = new System.Drawing.Point(148, 126);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(581, 379);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniProgram});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(832, 24);
+            this.mnuMenu.TabIndex = 2;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniProgram
+            // 
+            this.mniProgram.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniProgram.Name = "mniProgram";
+            this.mniProgram.Size = new System.Drawing.Size(65, 20);
+            this.mniProgram.Text = "Program";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(93, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // frmProgramMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 517);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblCallMeCarson);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmProgramMenu";
+            this.Text = "Program Menu By Noah.C";
+            this.Load += new System.EventHandler(this.FrmProgramMenu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCallMeCarson;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniProgram;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ ProgramMenuNoah/ProgramMenuNoah/ProgramMenuForm.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by: John Smith
+ * Created on: Feb. 7, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #4 - Hello World with Menu
+ * This program displays Hello World but with an exit menu
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramMenuNoah
+{
+    public partial class frmProgramMenu : Form
+    {
+        public frmProgramMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //this will close the program
+            this.Close();
+        }
+
+        private void FrmProgramMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
